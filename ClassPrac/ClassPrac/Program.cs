e08deb468e91805a73ea27e66005beb999d85794
Simeoncoded <simeonolawore07@gmail.com>
2025-04-19 16:32:50 -0400

    class pract

---
+++ ClassPrac/ClassPrac/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animalID = "00000001";
+            string species = "Dog";
+            char gender = 'M';
+            bool isSpayed = true;
+            string colour = "Brown";
+            DateTime birthday = new DateTime(2018, 5, 23);
+            string vaccineStatus = "Up to date";
+            string IdType = "Micro-chipped";
+            string IdNo = "MC12345678";
+            double adoptionFee = 200;
+        }
+    }
+}
+
