cd651d90f2418ab51df89ffe21680976cb44d80f
Simeoncoded <simeonolawore07@gmail.com>
2025-04-26 11:23:20 -0400

    update on pract

--- PetPract/PetPract/Pet.cs
+++ PetPract/PetPract/Pet.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace PetPract
 {
-    internal class Pet
+    public class Pet
     {
+        public int ID {get; set;}
+
+        public string Name {get; set;}  
+
+        public string Species {get; set;}
+
+        public DateTime CheckInTime { get; set;}
+
+        public bool isCheckedOut { get; set;}
+
+
+        public Pet (int id, string name, string species, DateTime checkInTime, bool ischeckedout)
+        {
+            ID = id;
+            Name = name;
+            Species = species;
+            CheckInTime = checkInTime;
+            isCheckedOut = ischeckedout;
+            
+
+        }
     }
 }
