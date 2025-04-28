961fb2c4546be92b802620cc1da70433054f8726
Simeoncoded <simeonolawore07@gmail.com>
2025-04-28 00:32:27 -0400

    update on pract

---
+++ ThePetApp/ThePetApp/PetLibrary/Pet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetLibrary
+{
+    public class Pet
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+
+        public DateTime CheckInTime { get; set; }
+
+        public bool isCheckedOut { get; set; }
+
+
+        public Pet(string id, string name, string species, DateTime checkInTime, bool ischeckedout)
+        {
+            ID = id;
+            Name = name;
+            Species = species;
+            CheckInTime = checkInTime;
+            isCheckedOut = ischeckedout;
+
+        }
+    }
+}
