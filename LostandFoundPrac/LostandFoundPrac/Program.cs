98e00683da5aa543bfa5673e7033cd3df7d48045
Simeoncoded <simeonolawore07@gmail.com>
2025-05-03 02:15:38 -0400

    update

--- LostandFoundPrac/LostandFoundPrac/Program.cs
+++ LostandFoundPrac/LostandFoundPrac/Program.cs
@@ -12,6 +12,17 @@ namespace LostandFoundPrac
         static string filepath = "pets.txt";
         static void Main(string[] args)
         {
+            bool exit = false;
+
+            while (!exit) {
+                Console.WriteLine("\nWelcome to the Lost and found app");
+                Console.WriteLine("Press R to report a lost pet");
+                Console.WriteLine("Press F to report a found pet");
+                Console.WriteLine("Press U to update a pet");
+                Console.WriteLine("Press D to remove a pet");
+                Console.WriteLine("Press V to view all pets");
+                Console.WriteLine("Press E to exit app");
+            }
         }
     }
 }
