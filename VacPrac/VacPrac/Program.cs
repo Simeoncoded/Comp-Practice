ce5dab42b45e9e7f08652d6d62827b689be5d030
Simeoncoded <simeonolawore07@gmail.com>
2025-05-03 22:35:53 -0400

    update on pract

--- VacPrac/VacPrac/Program.cs
+++ VacPrac/VacPrac/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace VacPrac
 {
+   
     internal class Program
     {
+        static List<Vac> vac = new List<Vac>();
+
+        static string filepath = "vac.txt";
         static void Main(string[] args)
         {
+            Console.Write("\n Welcome to Vac app\n");
+            Console.Write("Press A to add new Animal");
+            Console.Write("Press M to mark animal as vaccinated");
+            Console.Write("Press V to view All Animal");
+            Console.Write("Press E to exit app");
+
+            bool exit = false;
+
+            while (!exit) { 
+               string choice = Console.ReadLine().ToUpper();
+
+                if (choice == "A")
+                {
+
+                }else if(choice == "M")
+                {
+
+                }else if(choice == "V")
+                {
+
+                }else if(choice == "E")
+                {
+                    exit = true;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice");
+                }
+
+            }
         }
     }
 }
