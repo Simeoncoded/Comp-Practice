1c8681ed8592bd476d3f8a6af59b88203fcac2c9
Simeoncoded <simeonolawore07@gmail.com>
2025-05-03 22:07:57 -0400

    update on pract

---
+++ VacPrac/VacPrac/Vac.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacPrac
+{
+    public class Vac
+    {
+        private string id;
+
+        public string ID
+        {
+            get
+            {
+             return id;
+            }
+
+            set { 
+                if(value.Length != 8)
+                {
+                    throw new Exception("ID shouldnt be more than 8 in length");
+                }
+                else
+                {
+                    id = value;
+                }
+                
+            }
+        }
+
+        public string Name { get; set; }
+
+        public string Species { get; set; }
+
+        public bool isVaccinated { get; set; }
+    }
+}
